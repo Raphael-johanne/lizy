a4db02986040666bf75f90bda87143c0c5267c22
Raphael Colboc <racol@smile.fr>
2016-03-29 23:50:03 +0200

    clean-project

---
+++ locales/fr.js
@@ -0,0 +1,12 @@
+{
+	"Lizy": "Lizy",
+	"Dashboard": "Dashboard",
+	"Products": "Products",
+	"Variants": "Variants",
+	"Categories": "Categories",
+	"Attributes": "Attributes",
+	"Families": "Families",
+	"Jobs": "Jobs",
+	"Import": "Import",
+	"Export": "Export"
+}
