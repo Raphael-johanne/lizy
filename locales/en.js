5f951aef2570dd297693fb874e4167e0ba472b66
Raphael <rcolboc.dev@gmail.com>
2016-05-19 00:03:39 +0200

    media folder git exclusion

--- locales/en.js
+++ locales/en.js
@@ -1,1 +1,11 @@
-{}
+{
+	"Lizy": "Lizy",
+	"Dashboard": "Dashboard",
+	"Products": "Products",
+	"Categories": "Categories",
+	"Attributes": "Attributes",
+	"Families": "Families",
+	"Jobs": "Jobs",
+	"Import": "Import",
+	"Export": "Export"
+}
